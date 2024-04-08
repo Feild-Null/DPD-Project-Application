c3d58a716f3c31f0a990e717e80bb98055b1c876
STUDENT\IW211506 <ianwight123@gmail.com>
2024-04-08 15:08:56 -0500

    Added Main Menu and Main Traffic Scenes

---
+++ Assets/Scripts/TrafficStopMain.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class TrafficStopMain : MonoBehaviour
+{
+    public void Passenger()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Driver()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
+//Change
